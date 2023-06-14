137397b9d5da61f45c40ff5136342c584ba8f81e
ubyhzargam <ubyhzargam@gmail.com>
2023-06-14 15:49:49 +0530

    Update main.cpp

--- tower_of_hanoi_using_recursion/main.cpp
+++ tower_of_hanoi_using_recursion/main.cpp
@@ -6,9 +6,21 @@
 //
 
 #include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+void hanoi(int n,int A, int B, int C)
+{
+    if(n>0)
+    {
+        hanoi(n-1,1,3,2);
+        cout<<"Move disc from "<<A<<" to "<<C<<endl;
+        hanoi(n-1,2,1,3);
+    }
+}
+int main()
+{
+    int n;
+    cout<<"Enter the number of discs to solve the tower of hanoi problem : ";
+    cin>>n;
+    hanoi(n,1,2,3);
     return 0;
 }
